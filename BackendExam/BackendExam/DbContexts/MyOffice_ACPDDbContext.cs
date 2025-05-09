9c70b4ca311f6d4642386c1d5f890b2d512e7b07
a80764205 <j800226@gmail.com>
2025-05-09 23:43:20 +0800

    改用SQL CONNECTION

--- BackendExam/BackendExam/DbContexts/MyOffice_ACPDDbContext.cs
+++ BackendExam/BackendExam/DbContexts/MyOffice_ACPDDbContext.cs
@@ -1,12 +1,139 @@
 ﻿using BackendExam.Models;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace BackendExam.DbContexts
 {
-    public class MyOffice_ACPDDbContext : DbContext
+    public class MyOffice_ACPDDbContext(string connectionString)
     {
-        public MyOffice_ACPDDbContext(DbContextOptions<MyOffice_ACPDDbContext> options) : base(options) { }
+        private readonly string _connectionString = connectionString;
+
+        public async Task<List<MyOffice_ACPDModel>> GetAsync(string json)
+        {
+            var result = new List<MyOffice_ACPDModel>();
+            using (var con = new SqlConnection(_connectionString))
+            using (var cmd = new SqlCommand("sp_MyOffice_ACPD_Select", con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@JsonString", json);
+
+                await con.OpenAsync();
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        result.Add(new MyOffice_ACPDModel
+                        {
+                            ACPD_SID = reader["ACPD_SID"].ToString(),
+                            ACPD_Cname = reader["ACPD_Cname"].ToString(),
+                            ACPD_Ename = reader["ACPD_Ename"].ToString(),
+                            ACPD_Sname = reader["ACPD_Sname"].ToString(),
+                            ACPD_Email = reader["ACPD_Email"].ToString(),
+                            ACPD_Status = reader.GetByte(reader.GetOrdinal("ACPD_Status")),
+                            ACPD_Stop = reader.GetBoolean(reader.GetOrdinal("ACPD_Stop")),
+                            ACPD_StopMemo = reader["ACPD_StopMemo"].ToString(),
+                            ACPD_LoginID = reader["ACPD_LoginID"].ToString(),
+                            ACPD_LoginPWD = reader["ACPD_LoginPWD"].ToString(),
+                            ACPD_Memo = reader["ACPD_Memo"].ToString(),
+                            ACPD_NowDateTime = reader.IsDBNull(reader.GetOrdinal("ACPD_NowDateTime"))?(DateTime?)null:reader.GetDateTime(reader.GetOrdinal("ACPD_NowDateTime")),
+                            ACPD_NowID = reader["ACPD_NowID"].ToString(),
+                            ACPD_UPDDateTime = reader.IsDBNull(reader.GetOrdinal("ACPD_UPDDateTime"))?(DateTime?)null:reader.GetDateTime(reader.GetOrdinal("ACPD_UPDDateTime")),
+                            ACPD_UPDID = reader["ACPD_UPDID"].ToString()
+                        });
+                    }
+                }
+            }
+            return result;
+        }
+
+
+        public async Task<bool> InsertAsync(string json)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            using (var cmd = new SqlCommand("sp_MyOffice_ACPD_Update", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@JsonString", json);
+
+                await conn.OpenAsync();
+                using (var transaction = conn.BeginTransaction())
+                {
+                    cmd.Transaction = transaction;
+                    try
+                    {
+                        await cmd.ExecuteNonQueryAsync();
+                        transaction.Commit();
+                        return true;
+                    }
+                    catch
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public async Task<bool> UpdateAsync(string json)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            using (var cmd = new SqlCommand("sp_MyOffice_ACPD_Insert", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@JsonString", json);
+
+                await conn.OpenAsync();
+                using (var transaction = conn.BeginTransaction())
+                {
+                    cmd.Transaction = transaction;
+                    try
+                    {
+                        await cmd.ExecuteNonQueryAsync();
+                        transaction.Commit();
+                        return true;
+                    }
+                    catch
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public async Task<bool> DeleteAsync(string json)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            using (var cmd = new SqlCommand("sp_MyOffice_ACPD_Delete", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@JsonString", json);
+
+                await conn.OpenAsync();
+                using (var transaction = conn.BeginTransaction())
+                {
+                    cmd.Transaction = transaction;
+                    try
+                    {
+                        await cmd.ExecuteNonQueryAsync();
+                        transaction.Commit();
+                        return true;
+                    }
+                    catch
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+
+    }
+    /*
+    public class MyOffice_ACPDDbContext_EF : DbContext
+    {
+        public MyOffice_ACPDDbContext_EF(DbContextOptions<MyOffice_ACPDDbContext> options) : base(options) { }
 
         public DbSet<MyOffice_ACPDModel> MyOffice_ACPD_DbSet { get; set; }
 
@@ -78,6 +205,7 @@ namespace BackendExam.DbContexts
             }
 
         }
-
+    
     }
+    */
 }
