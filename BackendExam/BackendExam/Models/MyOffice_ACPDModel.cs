9587c9c0e2521ddd7d497e642b73d885bb10435e
a80764205 <j800226@gmail.com>
2025-05-09 21:45:27 +0800

    search ok condition沒指定

---
+++ BackendExam/BackendExam/Models/MyOffice_ACPDModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackendExam.Models
+{
+    public class MyOffice_ACPDModel
+    {
+        [Key]
+	    public string ACPD_SID { get; set; }
+	    public string ACPD_Cname { get; set; }
+        public string ACPD_Ename { get; set; }
+        public string ACPD_Sname { get; set; }
+        public string ACPD_Email { get; set; }
+        public int ACPD_Status { get; set; }
+        public bool ACPD_Stop { get; set; }
+        public string ACPD_StopMemo { get; set; }
+        public string ACPD_LoginID { get; set; }
+        public string ACPD_LoginPWD { get; set; }
+        public string ACPD_Memo { get; set; }
+        public DateTime ACPD_NowDateTime { get; set; }
+        public string ACPD_NowID { get; set; }
+        public DateTime ACPD_UPDDateTime { get; set; }
+        public string ACPD_UPDID { get; set; }
+    }
+}
--- BackendExam/BackendExam/Program.cs
+++ BackendExam/BackendExam/Program.cs
@@ -1,7 +1,21 @@
+using BackendExam.DbContexts;
+using BackendExam.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Get Connection String
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//Register Service
+builder.Services.AddScoped<MyOffice_ACPDService>();
+
+//Register DbContext
+builder.Services.AddDbContext<MyOffice_ACPDDbContext>(options =>
+    options.UseSqlServer(connectionString)
+);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
